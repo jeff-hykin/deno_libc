7ec991e3742ecfa0f0eb5425d3d938ed3aae66f6
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-22 11:54:43 -0600

    -

--- tooling/macos/get_linked_libs.js
+++ tooling/macos/get_linked_libs.js
@@ -10,7 +10,7 @@ export function getLinkedLibs(fileBytes) {
     const offsetToName = 20 // NOTE: this is from observation, not from documentation. I think this is always the same for "reexport_dylib" segments
     let outputPaths = []
     for (let { data } of reExportedThings) {
-        const nameData = new Uint8Array(data).slice(20,)
+        const nameData = data.slice(offsetToName,)
         const pathString = (new TextDecoder().decode(nameData)).replace(/\x00+$/g,"") // chop off null terminator(s)
     }
     return outputPaths
