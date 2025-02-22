71a4d00db52d0638bbc395c7736ce4db73c56bc2
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-22 11:07:38 -0600

    create getLinkedLibs

---
+++ tooling/macos/get_linked_libs.js
@@ -0,0 +1,17 @@
+import { parse as parseMachO } from "./mach_o.js"
+
+var toStr = (arr)=>new TextDecoder().decode(new Uint8Array(arr))
+export function getLinkedLibs(fileBytes) {
+    const info = parseMachO(fileBytes)
+    const linkedFiles = info.cmds.filter(each=>each.type == "reexport_dylib")
+    
+    new TextDecoder().decode(new Uint8Array(info.cmds.filter(each=>each.type == "uuid")[0].data))
+    linkedFiles.map(each=>new Uint8Array(each.data))
+    const offsetToName = 20 // NOTE: this is from observation, not from documentation. I think this is always the same for "reexport_dylib" segments
+    let outputPaths = []
+    for (let { data } of linkedFiles) {
+        const nameData = new Uint8Array(data).slice(20,)
+        const pathString = (new TextDecoder().decode(nameData)).replace(/\x00+$/g,"") // chop off null terminator(s)
+    }
+    return outputPaths
+}
